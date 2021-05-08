9f19ea68ae981b88569504e619b07c978f567404
Igor Smirnov <igorsmirnov.gg@gmail.com>
2021-05-09 00:32:40 +0800

    updated component size

--- src/pages/Dex/SwapConsole.tsx
+++ src/pages/Dex/SwapConsole.tsx
@@ -28,7 +28,7 @@ import {
   InputHeader,
   LightHeader,
   TokenInputAmount,
-  TokenInputWrapper
+  TokenInputWrapper,
 } from './components'
 import TokenSelector from './components/TokenSelector'
 
--- src/pages/Dex/components/index.tsx
+++ src/pages/Dex/components/index.tsx
@@ -4,7 +4,7 @@ export const TokenInputAmount = styled.input`
   border: transparent;
   border-radius: 8px 0px 0px 8px;
   display: block;
-  padding: 0.75rem;
+  padding-left: 1rem;
   font-weight: 400;
   font-size: 15px;
   line-height: 1.5;
