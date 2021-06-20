3c6035f4d0be4e888935d6d2859d8f3cf45dc281
Igor Smirnov <igorsmirnov.gg@gmail.com>
2021-06-21 05:35:05 +0800

    updated projects page to be ordered by assets

--- src/pages/BulletTrain/Stats.tsx
+++ src/pages/BulletTrain/Stats.tsx
@@ -1,88 +1,41 @@
-import { ThinShadowCard } from 'components/Card'
-import { TokenText } from 'components/Text'
+import { RowFixed } from 'components/Row'
+import { HeavyText, SText, TokenText } from 'components/Text'
 import useStats from 'hooks/useStats'
 import { useSubstrate } from 'hooks/useSubstrate'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
 import { formatToUnit } from 'utils/formatUnit'
-
-const StatValue = styled.div`
-  font-size: 22px;
-  font-weight: 700;
-  color: #fff;
-
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-    font-size: 18px;
-    font-weight: 700;
-    color: #fff;
-  `};
-`
-
-const StatText = styled.div`
-  color: #000;
-  font-size: 12px;
-
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-    font-size: 11px;
-    font-weight: 400;
-`};
-`
-
-const StatDisplayContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-const StatDisplay = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, minmax(0, 4fr));
-  grid-template-rows: repeat(2, minmax(0, 4fr));
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
-
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-    display:grid;
-    grid-template-columns: repeat(2, minmax(0, 4fr));
-    grid-template-rows: repeat(2, minmax(0, 4fr));
-    grid-column-gap: 5px;
-    grid-row-gap: 5px;
-    width: 100%;
-  `};
-`
 
 export default function BulletTrainStats({ token }: { token: string }) {
   const stats = useStats(token)
   const { chainDecimals } = useSubstrate()
   const { t } = useTranslation()
-  const background = 'linear-gradient(90deg, #FFBE2E -11.67%, #EC3D3D 100%)'
   return (
-    <StatDisplayContainer>
-      <StatDisplay>
-        <ThinShadowCard background={background}>
-          <StatValue>{stats.totalCabinsBought}</StatValue>
-          <StatText>{t(`Cabins Purchased`)}</StatText>
-        </ThinShadowCard>
-        <ThinShadowCard background={background}>
-          <StatValue>{stats.totalPassengers}</StatValue>
-          <StatText>{t(`Passengers Aboard`)}</StatText>
-        </ThinShadowCard>
-        <ThinShadowCard background={background}>
-          <StatValue>
-            {formatToUnit(stats.totalYieldWithdrawn, chainDecimals, 0, true)}{' '}
-            <TokenText color="white">{token}</TokenText>
-          </StatValue>
-          <StatText>{t(`Yield Distributed`)}</StatText>
-        </ThinShadowCard>
-        <ThinShadowCard background={background}>
-          <StatValue>
-            {formatToUnit(stats.totalValueLocked, chainDecimals, 0, true)}{' '}
-            <TokenText color="white">{token}</TokenText>
-          </StatValue>
-          <StatText>{t(`Ticket Fares Stored`)}</StatText>
-        </ThinShadowCard>
-      </StatDisplay>
-    </StatDisplayContainer>
+    <>
+      <RowFixed>
+        <HeavyText color="#fff">{t(`Total Deposits Stored`)}:</HeavyText>
+        <SText padding="0 0.5rem" colorIsPrimary>
+          {formatToUnit(stats.totalValueLocked, chainDecimals)} <TokenText>{token}</TokenText>
+        </SText>
+      </RowFixed>
+      <RowFixed>
+        <HeavyText color="#fff">{t(`Yield Distributed`)}:</HeavyText>
+        <SText padding="0 0.5rem" colorIsPrimary>
+          {formatToUnit(stats.totalYieldWithdrawn, chainDecimals)} <TokenText>{token}</TokenText>
+        </SText>
+      </RowFixed>
+      <RowFixed>
+        <HeavyText color="#fff">{t(`Cabins Purchased`)}:</HeavyText>
+        <SText padding="0 0.5rem" colorIsPrimary>
+          {stats.totalCabinsBought}
+        </SText>
+      </RowFixed>
+      <RowFixed>
+        <HeavyText color="#fff">{t(`Users Participating`)}:</HeavyText>
+        <SText padding="0 0.5rem" colorIsPrimary>
+          {stats.totalPassengers}
+        </SText>
+      </RowFixed>
+    </>
   )
 }
--- src/pages/Projects/Project/index.tsx
+++ src/pages/Projects/Project/index.tsx
@@ -1,57 +1,34 @@
-import Card from 'components/Card'
-import { SText, Header2, Heading, ItalicText, HeavyText } from 'components/Text'
+import BulletTrainBanner from 'assets/images/banner-bullettrain.jpg'
+import DexIcon from 'assets/svg/icon-exchange.svg'
+import ChartIcon from 'assets/svg/icon-line-chart.svg'
+import BN from 'bn.js'
+import Card, { BannerCard } from 'components/Card'
+import PriceChart from 'components/Chart'
+import Divider from 'components/Divider'
+import { Icon } from 'components/Image'
+import { RowBetween } from 'components/Row'
+import { Header1, Header2, Header4, HeavyText, ItalicText, SText } from 'components/Text'
 import { BorderedWrapper, ContentWrapper, PageWrapper, Section, SpacedSection, Wrapper } from 'components/Wrapper'
 import { useProjectPath } from 'hooks/useProject'
 import useProjectInfos, { ProjectInfo } from 'hooks/useProjectInfo'
+import { usePoolsWithToken } from 'hooks/useQueryDexPool'
+import useStats from 'hooks/useStats'
 import { useSubstrate } from 'hooks/useSubstrate'
+import BulletTrainStats from 'pages/BulletTrain/Stats'
 import React, { useEffect, useMemo, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 import { formatToUnit } from 'utils/formatUnit'
 import getProjectRegistry from 'utils/getProjectRegistry'
-import BulletTrainImage from 'assets/images/bullettrain-vector.png'
-import DexIcon from 'assets/svg/icon-exchange.svg'
-import BulletTrainIcon from 'assets/svg/icon-train-yellow.svg'
-import ChartIcon from 'assets/svg/icon-line-chart.svg'
-import BulletTrainStats from 'pages/BulletTrain/Stats'
-import { useTranslation } from 'react-i18next'
-import { usePoolsWithToken } from 'hooks/useQueryDexPool'
-import { RowBetween } from 'components/Row'
-import PriceChart from 'components/Chart'
-import useStats from 'hooks/useStats'
-import BN from 'bn.js'
 
 const GridWrapper = styled.div<{ columns?: string; mobileColumns?: string }>`
   display: grid;
   grid-template-columns: auto 3fr;
   grid-column-gap: 1rem;
+  align-items: center;
 
   ${({ theme }) => theme.mediaWidth.upToMedium`
   grid-column-gap: 0.5rem;
-  `};
-`
-
-const ProjectIconWrapper = styled.div`
-  display: flex;
-  max-width: 120px;
-  justify-content: center;
-  padding: 1rem;
-  align-items: center;
-  justify-content: center;
-  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-    max-width: 80px;
-    padding: 0.5rem;
-  `};
-`
-
-const BannerGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, minmax(0, 4fr));
-  grid-column-gap: 1rem;
-  grid-row-gap: 1rem;
-
-  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-  display:grid;
-  grid-template-columns: repeat(1, minmax(0, 4fr));
   `};
 `
 
@@ -63,6 +40,62 @@ const TokenGrid = styled.div`
   width: 100%;
 `
 
+function TokenPerformance({ projectInfo }: { projectInfo: ProjectInfo }) {
+  const { t } = useTranslation()
+  const [priceAvailable, setPriceAvailable] = useState<boolean>(true)
+  const [latestPrice, setLatestPrice] = useState<string>('')
+  const [token1, token2] = useMemo(() => {
+    if (!projectInfo) return [undefined, undefined]
+    if (projectInfo.token === 'BOLT') {
+      return ['BOLT', 'WUSD']
+    } else {
+      return ['WUSD', projectInfo.token]
+    }
+  }, [projectInfo])
+  return (
+    <>
+      {projectInfo && (
+        <ContentWrapper>
+          <Card>
+            <Header2 style={{ display: 'inline-flex' }}>
+              <div style={{ display: 'block', maxWidth: '25px', marginRight: '0.5rem' }}>
+                <img alt="Price Chart" style={{ display: 'block', width: '100%' }} src={ChartIcon} />
+              </div>
+              {t(`Token Performance`)}
+            </Header2>
+            <SpacedSection>
+              {latestPrice && (
+                <>
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <SText>{t(`Current Price`)}</SText>
+                  </div>
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <HeavyText fontSize="28px" mobileFontSize="24px" style={{ paddingRight: '1rem' }}>
+                      ${latestPrice}
+                    </HeavyText>
+                    <HeavyText>{`${projectInfo.token.toUpperCase()} / WUSD `}</HeavyText>
+                  </div>
+                </>
+              )}
+            </SpacedSection>
+            {token1 && token2 && (
+              <PriceChart
+                token1={token1}
+                token2={token2}
+                from={0}
+                interval={300}
+                setAvailable={setPriceAvailable}
+                setLatestPrice={setLatestPrice}
+              />
+            )}
+            {!priceAvailable && <div>{`Price is unavailable for this token`}</div>}
+          </Card>
+        </ContentWrapper>
+      )}
+    </>
+  )
+}
+
 export default function Project(): JSX.Element {
   const projectPath = useProjectPath()
   const projectInfos = useProjectInfos()
@@ -73,9 +106,8 @@ export default function Project(): JSX.Element {
   const TokenImage = require(`assets/tokens/${projectRegistry.icon}`)
   const { t } = useTranslation()
   const dexPools = usePoolsWithToken(projectPath.token)
-  const [priceAvailable, setPriceAvailable] = useState<boolean>(true)
+
   const stats = useStats(projectPath.token.toUpperCase())
-  const [latestPrice, setLatestPrice] = useState<string>('')
 
   const noBulletTrain =
     stats.totalCabinsBought === 0 &&
@@ -89,32 +121,23 @@ export default function Project(): JSX.Element {
     setProjectInfo(currentProject)
   }, [projectInfos, projectPath])
 
-  const [token1, token2] = useMemo(() => {
-    if (!projectInfo) return [undefined, undefined]
-    if (projectInfo.token === 'BOLT') {
-      return ['BOLT', 'WUSD']
-    } else {
-      return ['WUSD', projectInfo.token]
-    }
-  }, [projectInfo])
+  // const assetFilter = useMemo(() => {
+  //   const options = ['ALL', 'CREATED', 'ACTIVE', 'RUNNING', 'COMPLETED', 'EXPIRED', 'FAILED']
+  //   return options.map((label) => ({ label, callback: () => setFilteredState(label) }))
+  // }, [])
 
   return (
     <>
       <PageWrapper style={{ maxWidth: '680px' }}>
         {projectPath && (
           <>
-            <Card style={{ width: '100%', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-              <Heading style={{ paddingTop: '0.5rem' }}>{t(`Project Info`)}</Heading>
+            <Card borderRadius="0">
               <GridWrapper columns={'2'} mobileColumns={'2'}>
-                <BorderedWrapper style={{ display: 'flex' }}>
-                  <ProjectIconWrapper>
-                    <img src={TokenImage} style={{ width: '100%' }} alt="project logo" />
-                  </ProjectIconWrapper>
-                </BorderedWrapper>
+                <Icon src={TokenImage} size="60px" mobileSize="40px" alt="project logo" />
                 {projectInfo && (
                   <ContentWrapper>
                     <SpacedSection>
-                      <Header2>{projectRegistry.name}</Header2>
+                      <Header1>{projectRegistry.name}</Header1>
                       <ItalicText>{projectRegistry.description}</ItalicText>
                       <SpacedSection>
                         <SText>
@@ -136,81 +159,23 @@ export default function Project(): JSX.Element {
                 )}
               </GridWrapper>
             </Card>
-
-            {projectInfo && (
-              <ContentWrapper>
-                <Card>
-                  <Header2 style={{ display: 'inline-flex' }}>
-                    <div style={{ display: 'block', maxWidth: '25px', marginRight: '0.5rem' }}>
-                      <img alt="Price Chart" style={{ display: 'block', width: '100%' }} src={ChartIcon} />
-                    </div>
-                    {t(`Token Performance`)}
-                  </Header2>
-                  <SpacedSection>
-                    {latestPrice && (
-                      <>
-                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                          <SText>{t(`Current Price`)}</SText>
-                        </div>
-                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                          <HeavyText fontSize="28px" mobileFontSize="24px" style={{ paddingRight: '1rem' }}>
-                            ${latestPrice}
-                          </HeavyText>
-                          <HeavyText>{`${projectInfo.token.toUpperCase()} / WUSD `}</HeavyText>
-                        </div>
-                      </>
-                    )}
-                  </SpacedSection>
-                  {token1 && token2 && (
-                    <PriceChart
-                      token1={token1}
-                      token2={token2}
-                      from={0}
-                      interval={300}
-                      setAvailable={setPriceAvailable}
-                      setLatestPrice={setLatestPrice}
-                    />
-                  )}
-                  {!priceAvailable && <div>{`Price is unavailable for this token`}</div>}
-                </Card>
-              </ContentWrapper>
-            )}
-
+            {/* <Filter  /> */}
             {!noBulletTrain && (
               <ContentWrapper>
-                <Wrapper
-                  style={{
-                    display: 'flex',
-                    width: '100%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Card>
-                    <Section style={{ marginBottom: '1rem' }}>
-                      <Header2 style={{ display: 'inline-flex' }}>
-                        <div style={{ display: 'block', maxWidth: '25px', marginRight: '0.5rem' }}>
-                          <img
-                            alt="BulletTrain icon"
-                            style={{ display: 'block', width: '100%' }}
-                            src={BulletTrainIcon}
-                          />
-                        </div>
-                        {t(`BulletTrain Performance`)}
-                      </Header2>
-                    </Section>
-                    <Section style={{ width: '100%' }}>
-                      <BannerGrid style={{ width: '100%', marginBottom: '1rem' }}>
-                        <div style={{ maxWidth: '250px', justifyContent: 'left' }}>
-                          <img alt="BulletTrain banner" style={{ width: '100%' }} src={BulletTrainImage} />
-                        </div>
-                        {projectInfo && <BulletTrainStats token={projectInfo.token} />}
-                      </BannerGrid>
-                    </Section>
-                  </Card>
-                </Wrapper>
+                <BannerCard url={BulletTrainBanner} padding="3rem 1rem" darkenBackground>
+                  <Header2 colorIsPrimary>{t(`BulletTrain TravelCabins`)}</Header2>
+                  <Header4 color="#fff">
+                    {`${t(`Earn by depositing your tokens and referring your friends`)}. 
+                    ${t(`Click to get more tokens`)}!`}
+                  </Header4>
+                  <Divider margin="0.5rem 0" />
+                  <Section style={{ width: '100%' }}>
+                    {projectInfo && <BulletTrainStats token={projectInfo.token} />}
+                  </Section>
+                </BannerCard>
               </ContentWrapper>
             )}
+            {projectInfo && <TokenPerformance projectInfo={projectInfo} />}
 
             <ContentWrapper>
               <Wrapper
