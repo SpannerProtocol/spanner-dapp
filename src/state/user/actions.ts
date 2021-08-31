6520fea2f7f855d5a397e8bcf01862faa2209485
Lint Action <lint-action@samuelmeuli.com>
2021-08-31 00:57:37 +0000

    Fix code style issues with ESLint

--- src/state/user/actions.ts
+++ src/state/user/actions.ts
@@ -23,7 +23,8 @@ export const updateUserDeadline = createAction<{ userDeadline: number }>('user/u
 export const addSerializedToken = createAction<{ serializedToken: SerializedToken }>('user/addSerializedToken')
 export const removeSerializedToken = createAction<{ chainId: number; address: string }>('user/removeSerializedToken')
 export const addSerializedPair = createAction<{ serializedPair: SerializedPair }>('user/addSerializedPair')
-export const removeSerializedPair =
-  createAction<{ chainId: number; tokenAAddress: string; tokenBAddress: string }>('user/removeSerializedPair')
+export const removeSerializedPair = createAction<{ chainId: number; tokenAAddress: string; tokenBAddress: string }>(
+  'user/removeSerializedPair'
+)
 export const toggleURLWarning = createAction<void>('app/toggleURLWarning')
 export const updateE2sTs = createAction<void>('user/updateE2sTs')
